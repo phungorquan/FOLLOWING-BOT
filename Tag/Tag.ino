96b87a7bfaa870e011fe2c0d6409d8bf2373ea06
Tong Anh Quan <anhquantong77@gmail.com>
2018-11-13 23:57:37 +0700

    Add

---
+++ Tag/Tag.ino
@@ -0,0 +1,71 @@
+/**
+ * 
+ * @todo
+ *  - move strings to flash (less RAM consumption)
+ *  - fix deprecated convertation form string to char* startAsTag
+ *  - give example description
+ */
+#include <SPI.h>
+#include "DW1000Ranging.h"
+#include <DW1000.h>
+// connection pins
+const uint8_t PIN_RST = 9; // reset pin
+const uint8_t PIN_IRQ = 3; // irq pin
+const uint8_t PIN_SS = SS; // spi select pin
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+   // DW1000.begin(PIN_IRQ, PIN_RST);
+ // DW1000.select(PIN_SS);
+  
+ //uint8_t a = 'BB';
+  
+  //init the configuration
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+  //define the sketch as anchor. It will be great to dynamically change the type of module
+//   Serial.println(F("DW1000 initialized ..."));
+//  // general configuration
+//  DW1000.newConfiguration();
+//  DW1000.setDeviceAddress(5);
+//  DW1000.setNetworkId(10);
+//  DW1000.commitConfiguration();
+//  Serial.println(F("Committed configuration ..."));
+//  // wait a bit
+//  delay(1000);
+  //DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+  //Enable the filter to smooth the distance
+  DW1000Ranging.useRangeFilter(true);
+  
+  //we start the module as a tag
+  DW1000Ranging.startAsTag("BB:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY,false);
+}
+
+void loop() {
+  DW1000Ranging.loop();
+}
+
+void newRange() {
+  //if(DW1000Ranging.getDistantDevice()->getShortAddress() == 0xA1) // if Anchor is A1 
+ // {
+  Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m");
+  Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+  //}
+  
+}
+
+void newDevice(DW1000Device* device) {
+  Serial.print("ranging init; 1 device added ! -> ");
+  Serial.print(" short:");
+  //if(device->getShortAddress() == "BB");
+  Serial.println(device->getShortAddress(), HEX);
+}
+
+void inactiveDevice(DW1000Device* device) {
+  Serial.print("delete inactive device: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
+
